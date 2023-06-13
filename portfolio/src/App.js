a6c4186cee7c40ea849f63d0bf8ed42e2f6c01a6
Jesus <jesusc14@uw.edu>
2023-06-13 13:17:41 -0700

    added react

---
+++ portfolio/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import MenuIcon from '@mui/icons-material/Menu';
+import ButtonAppBar from './ButtonAppBar';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonAppBar /> {/* Add the ButtonAppBar component here */}
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
